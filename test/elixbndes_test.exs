42976089d4a4e0bdf854ddc206391a4b9fe62336
joao2391 <j_paulo_15@hotmail.com>
2022-04-17 11:23:27 -0300

    feat: add teste unitario

--- test/elixbndes_test.exs
+++ test/elixbndes_test.exs
@@ -10,8 +10,13 @@ defmodule ElixBndesTest do
     assert ElixBndes.get_fornecedores_by_nome("Zezinho", 1) |> Enum.count() == 9
   end
 
-  test "retorna 09 fornecedores" do
+  test "retorna 25 fornecedores" do
     assert ElixBndes.get_fornecedores_by_nome_produto("cimento", 1) |> Enum.count() == 25
+  end
+
+  test "retorna 10 itens" do
+    lista = ElixBndes.get_produtos_by_nome("cimento",1)
+    assert lista.itens |> Enum.count() == 10
   end
 
 end
